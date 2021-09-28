c03ec501dc2b539842f15cd19abe3424bae29192
divious1 <josehelps@gmail.com>
2021-09-28 13:35:21 -0400

    restoring securitytrails

--- modules/securitytrails.py
+++ modules/securitytrails.py
@@ -13,19 +13,22 @@ def search(search, API_KEY, log):
     total_results = r_json['total']['value']
     total_pages = round(total_results/100)
     # need to caculate pages from here
-    log.info('Securitytrails total results: {0}'.format(total_results))
-    log.info("Processing page: {0} out of {1}".format(page_number,total_pages))
+    log.info('securitytrails total results: {0}'.format(total_results))
+    log.info("processing page: {0} out of {1}".format(page_number,total_pages))
 
-    for page_number in range(2, total_pages):
-        if (r_json['total']['value'] > 100):
-            r = requests.get('https://api.securitytrails.com/v1/scroll/<CAN I PASS IT THE PAGE HERE?>' + r_json['id'], headers=headers)
-            r_json = r.json()
-            for result in r_json['records']:
-                open_instance = dict()
-                if result['ports']['port']:
-                    log.debug("Found matching {0}:{1}".format(result['address'],",".join(str(i) for i in result['ports']['port'])))
-                    open_instance ['ip'] = result['address']
-                    open_instance['port'] = ",".join(str(i) for i in result['ports']['port'])
-                    open_instances.append(open_instance)
+    #print(json.dumps(r_json))
 
+    for result in r_json['records']:
+        open_instance = dict()
+        if result['ports']['port']:
+            log.debug("found matching {0}:{1}".format(result['address'],",".join(str(i) for i in result['ports']['port'])))
+            open_instance ['ip'] = result['address']
+            open_instance['port'] = ",".join(str(i) for i in result['ports']['port'])
+            open_instances.append(open_instance)
+
+    #for page_number in range(2, total_pages):
+    #if (r_json['total']['value'] > 100):
+    #    r = requests.get('https://api.securitytrails.com/v1/scroll/' + r_json['id'], headers=headers, data=json.dumps(data))
+    #    r_json = r.json()
+    #    print(r_json)
     return open_instances
